86db4dfcf6232dc0741277dd5a5c86fe244fb28b
Callum <callum.re.anderson@gmail.com>
2025-05-23 16:40:36 +0000

    auto commit not working still

--- commit_change.sh
+++ commit_change.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+echo "This is an attempt to commit" >> commit.log
+
 # arg $0 is script name, same as python
 
 if [ $# -eq 0 ]
   then
-    echo "not enough arguments, missing commit message"
+    echo "not enough arguments, missing commit message" >> commit.log
     exit 1
 fi
 
-git add ./pages/*
+git add ./pages/* >> commit.log
 
-git commit -m "$1"
+git commit -m "$1" >> commit.log
+
+echo "Success" >> commit.log
 
 exit 0
