6eb9dc0a4e4647103b86c41b7a6afe1a52e802e2
Callum Anderson <callum.re.anderson@gmail.com>
2025-05-23 17:51:50 +0100

    asodasd

--- commit_change.sh
+++ commit_change.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo "This is an attempt to commit" >> commit.log
+echo "This is an attempt to commit" >> autocommit.log
 
 # arg $0 is script name, same as python
 
 if [ $# -eq 0 ]
   then
-    echo "not enough arguments, missing commit message" >> commit.log
+    echo "not enough arguments, missing commit message" >> autocommit.log
     exit 1
 fi
 
-git add ./pages/* >> commit.log
+git add ./pages/* >> autocommit.log
 
-git commit -m "$1" >> commit.log
+git commit -m "$1" >> autocommit.log
 
-echo "Success" >> commit.log
+echo "Success" >> autocommit.log
 
 exit 0
