c55b445d6ac6cf727b9cf2a4057f9576250378fd
Callum Anderson <callum.re.anderson@gmail.com>
2025-05-25 15:18:18 +0100

    maybe Id injection is working?

--- markdown_processor.js
+++ markdown_processor.js
@@ -7,9 +7,10 @@ function make_toc(){
 }
 
 function inject_header_ids(content){
-    var h1_tags = content.querySelectorAll("h1, h2")/*getElementsByTagName("h1") + content.getElementsByTagName("h2")*/
-    for(var id_count = 0; id_count < h1_tags.length; id_count++){
-        console.log(h1_tags[id_count])
+    var h12_tags = content.querySelectorAll("h1, h2")/*getElementsByTagName("h1") + content.getElementsByTagName("h2")*/
+    for(var id_count = 0; id_count < h12_tags.length; id_count++){
+        console.log(h12_tags[id_count])
+        h12_tags[id_count].id = h12_tags[id_count].innerHTML
     }
 }
 
