32b90a73b91a686329a75c6509012c1cfc7fc793
Callum Anderson <callum.re.anderson@gmail.com>
2025-05-25 15:06:43 +0100

    asdfg

--- markdown_processor.js
+++ markdown_processor.js
@@ -3,5 +3,11 @@ console.log("hi")
 document.getElementById("foot").innerHTML += "<h1>HI THERE</h1>";
 
 function make_toc(){
-    
+
 }
+
+function process_md(content){
+    console.log(content)
+}
+
+process_md(document.getElementById("content"))
