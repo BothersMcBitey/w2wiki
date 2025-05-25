daf087ad89a680edd336e2ac959e0ccde3a07b0e
Callum Anderson <callum.re.anderson@gmail.com>
2025-05-25 15:11:50 +0100

    Can we loop over h1 tags?

--- markdown_processor.js
+++ markdown_processor.js
@@ -6,6 +6,13 @@ function make_toc(){
 
 }
 
+function inject_header_ids(content){
+    h1_tags = content.getElementsByTagName("h1")
+    for(var id_count = 0; i < h1_tags.length; i++){
+        console.log(h1_tags[id_count])
+    }
+}
+
 function process_md(content){
     console.log(content)
 }
