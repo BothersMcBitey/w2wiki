d3befb14bd857d3fe39fb7663aaf8667f58c61ce
Callum Anderson <callum.re.anderson@gmail.com>
2025-05-26 18:29:46 +0100

    Both ends?

--- fancy_scroll.js
+++ fancy_scroll.js
@@ -5,12 +5,22 @@ function toggleScrollButton() {
   if (document.body.scrollTop > 160) { // || document.documentElement.scrollTop > 40) {
     //console.log("show")
     document.getElementById("top").style.visibility = "visible";
-    document.getElementById("bottom").style.visibility = "visible"
+    //document.getElementById("bottom").style.visibility = "visible"
     //jQuery(".float_button").show();
   } else {
     //jQuery("float_button").hide();
     //console.log("hide")
     document.getElementById("top").style.visibility = "hidden"
+    //document.getElementById("bottom").style.visibility = "hidden"
+  }
+
+  if (document.body.scrollTop < (document.body.scrollHeight - 100)) { // || document.documentElement.scrollTop > 40) {
+    //console.log("show")
+    document.getElementById("bottom").style.visibility = "visible"
+    //jQuery(".float_button").show();
+  } else {
+    //jQuery("float_button").hide();
+    //console.log("hide")
     document.getElementById("bottom").style.visibility = "hidden"
   }
 }
