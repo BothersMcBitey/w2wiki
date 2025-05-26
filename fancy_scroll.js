033613b8abbb531ee626763e9ef3c6d5d7eb3c79
Callum Anderson <callum.re.anderson@gmail.com>
2025-05-26 18:36:03 +0100

    maybe?

--- fancy_scroll.js
+++ fancy_scroll.js
@@ -19,7 +19,7 @@ function toggleScrollButton() {
   console.log(document.body.scrollHeight)
   console.log(document.body.clientHeight)
   console.log(document.body.scrollHeight - document.body.clientHeight)
-  if (document.body.scrollTop < (document.body.scrollHeight - (document.body.clientHeight - 50))) { // || document.documentElement.scrollTop > 40) {
+  if (document.body.scrollTop < (document.body.scrollHeight + (document.body.clientHeight + 50))) { // || document.documentElement.scrollTop > 40) {
     //console.log("show")
     document.getElementById("bottom").style.visibility = "visible"
     //jQuery(".float_button").show();
