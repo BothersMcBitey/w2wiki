3c5fa9afd187fd350fa4e6c8ec1323f8c4d42294
Callum <callum.re.anderson@gmail.com>
2025-05-26 17:22:34 +0000

    fixed scroll

--- fancy_scroll.js
+++ fancy_scroll.js
@@ -1,13 +1,16 @@
 window.onscroll = function() {toggleScrollButton()};
 
 function toggleScrollButton() {
-  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+  //console.log(document.body.scrollTop)
+  if (document.body.scrollTop > 160) { // || document.documentElement.scrollTop > 40) {
+    //console.log("show")
     document.getElementById("top").style.visibility = "visible";
     document.getElementById("bottom").style.visibility = "visible"
     //jQuery(".float_button").show();
   } else {
     //jQuery("float_button").hide();
-    document.getElementById("top").style.visibility = "none"
-    document.getElementById("bottom").style.visibility = "none"
+    //console.log("hide")
+    document.getElementById("top").style.visibility = "hidden"
+    document.getElementById("bottom").style.visibility = "hidden"
   }
 }
