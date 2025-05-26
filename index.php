ca7feb66dbc61f6a9d43148b4c39a0f53338126d
Callum Anderson <callum.re.anderson@gmail.com>
2025-05-26 18:47:10 +0100

    Moving buttons

--- index.php
+++ index.php
@@ -578,19 +578,19 @@ print "<div class=\"titlebar\">" .
 
 print "</header>\n";
 
+print "<main id=\"main\">\n";
 print "<div id=\"top\" class=\"float_button\">" . 
     	"<a href=\"#body\">Jump to Top</a>" .
 	  "</div>";
-
-print "<main id=\"main\">\n";
 print "<div id=\"markdown-content\" class=\"content\">\n";
 print "$html\n";
 print "</div>\n";
-print "</main>\n";
-
 print "<div id=\"bottom\" class=\"float_button\">" . 
     	"<a href=\"#foot\">Jump to Bottom</a>" .
 	  "</div>";
+print "</main>\n";
+
+
 
 print "<div class=\"padding\"></div>";
 
