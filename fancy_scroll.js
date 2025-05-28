a6abc607f782f88804f61764ba57eba7d8ac1dcc
Callum Anderson <callum.re.anderson@gmail.com>
2025-05-28 21:08:25 +0100

    Jumps don't work on edit page

--- fancy_scroll.js
+++ fancy_scroll.js
@@ -2,7 +2,7 @@ window.onscroll = function() {toggleScrollButton()};
 toggleScrollButton();
 
 function toggleScrollButton() {
-  //console.log(document.body.scrollTop)
+  console.log(document.body.scrollTop)
   if (document.body.scrollTop > 160) { // || document.documentElement.scrollTop > 40) {
     //console.log("show")
     document.getElementById("top").style.visibility = "visible";
