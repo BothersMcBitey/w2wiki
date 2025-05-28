fac310ef315a8c2070781609f4316c9358ae9969
Callum Anderson <callum.re.anderson@gmail.com>
2025-05-28 21:18:03 +0100

    Help there is bugs

--- fancy_scroll.js
+++ fancy_scroll.js
@@ -3,7 +3,7 @@ toggleScrollButton();
 
 function toggleScrollButton() {
   console.log(document.body.scrollTop)
-  if (document.body.scrollTop > 160) { // || document.documentElement.scrollTop > 40) {
+  if (document.body.scrollTop > 0) { // || document.documentElement.scrollTop > 40) {
     //console.log("show")
     document.getElementById("top").style.visibility = "visible";
     //document.getElementById("bottom").style.visibility = "visible"
