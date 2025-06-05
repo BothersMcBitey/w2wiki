a5eb430056d7103551c835b618f88e1f4ba16fb3
Callum Anderson <callum.re.anderson@gmail.com>
2025-06-05 17:38:41 +0100

    Only traps CTRL key combos specified, not all of them

--- trap_control.js
+++ trap_control.js
@@ -1,19 +1,11 @@
 //document.getElementById('text_edit').addEventListener('keydown', function(e) {
 document.addEventListener('keydown', function(e) {
-  if (e.key == 's' && e.ctrlKey) {
-    e.preventDefault();
-    console.log("Page Saved!")
-    var save_button = document.getElementById("save");
-    save_button.click();
-    /*var start = this.selectionStart;
-    var end = this.selectionEnd;
-
-    // set textarea value to: text before caret + tab + text after caret
-    this.value = this.value.substring(0, start) +
-      "\t" + this.value.substring(end);
-
-    // put caret at right position again
-    this.selectionStart =
-      this.selectionEnd = start + 1;*/
+  if (e.ctrlKey) {
+    if(e.key == 's'){
+      e.preventDefault();
+      console.log("Page Saved!")
+      var save_button = document.getElementById("save");
+      save_button.click();
+    }
   }
 });
