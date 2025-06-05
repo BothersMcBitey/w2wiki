586df36f31faf06b22762cfe33e579b7907b8808
Callum Anderson <callum.re.anderson@gmail.com>
2025-06-05 18:12:44 +0100

    Vried thicness lines

--- insert_horizontal_rules.js
+++ insert_horizontal_rules.js
@@ -1,7 +1,17 @@
 function inject_horizontal_rules(content){
-    var h_tags = content.querySelectorAll("h1, h2")    
-    for(var id_count = 0; id_count < h_tags.length; id_count++){
-        h_tags[id_count].after(document.createElement("hr"))
+    // major lines
+    var h1_tags = content.querySelectorAll("h1")    
+    for(var id_count = 0; id_count < h1_tags.length; id_count++){
+        let hr =document.createElement("hr");
+        hr.classList.add("major")
+        h1_tags[id_count].after(hr);
+    }
+    // minor lines
+    var h2_tags = content.querySelectorAll("h2")   
+    for(var id_count = 0; id_count < h2_tags.length; id_count++){
+        let hr =document.createElement("hr");
+        hr.classList.add("minor")
+        h2_tags[id_count].after(hr);
     }
 }
 
