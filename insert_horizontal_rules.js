74498847eb541bdfc329835ef04cde024456c404
Callum Anderson <callum.re.anderson@gmail.com>
2025-06-05 18:06:43 +0100

    ???

--- insert_horizontal_rules.js
+++ insert_horizontal_rules.js
@@ -1,8 +1,8 @@
 function inject_horizontal_rules(content){
     var h_tags = content.querySelectorAll("h1, h2")
     let hr = document.createElement("hr")
+    console.log(hr)
     for(var id_count = 0; id_count < h_tags.length; id_count++){
-
         h_tags[id_count].after(hr)
     }
 }
