cfbb3e371104bf6a4e677b0edf42d438a9d289be
Callum Anderson <callum.re.anderson@gmail.com>
2025-06-05 17:28:13 +0100

    attempt to trap ctrl+[key]

---
+++ trap_crtl_s.js
@@ -0,0 +1,16 @@
+document.getElementById('text').addEventListener('keydown', function(e) {
+  if (e.key == 's' && e.ctrlKey) {
+    e.preventDefault();
+    console.log("Page Saved!")
+    /*var start = this.selectionStart;
+    var end = this.selectionEnd;
+
+    // set textarea value to: text before caret + tab + text after caret
+    this.value = this.value.substring(0, start) +
+      "\t" + this.value.substring(end);
+
+    // put caret at right position again
+    this.selectionStart =
+      this.selectionEnd = start + 1;*/
+  }
+});
