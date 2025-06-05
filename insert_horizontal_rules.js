ebd04dfd866ecd287e6452169e2345d9b64b0299
Callum Anderson <callum.re.anderson@gmail.com>
2025-06-05 18:07:32 +0100

    Each one needs a new element, duh

--- insert_horizontal_rules.js
+++ insert_horizontal_rules.js
@@ -1,9 +1,9 @@
 function inject_horizontal_rules(content){
     var h_tags = content.querySelectorAll("h1, h2")
-    let hr = document.createElement("hr")
+    //let hr = document.createElement("hr")
     console.log(hr)
     for(var id_count = 0; id_count < h_tags.length; id_count++){
-        h_tags[id_count].after(hr)
+        h_tags[id_count].after(document.createElement("hr"))
     }
 }
 
