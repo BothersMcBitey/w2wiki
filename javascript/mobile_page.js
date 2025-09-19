502cefcbb84908454e05f88822c8e1790cd65e9b
Callum Anderson <callum.re.anderson@gmail.com>
2025-09-19 18:00:28 +0100

    I am dumb and my brain is square

--- javascript/mobile_page.js
+++ javascript/mobile_page.js
@@ -1,7 +1,7 @@
 function enable_modible_mode(){
     document.getElementById("logo").style.display = "none";
     document.getElementById("left").style.display = "none";
-    document.getElementById("left").style.display = "none";
+    document.getElementById("right").style.display = "none";
 }
 
 enable_modible_mode();
