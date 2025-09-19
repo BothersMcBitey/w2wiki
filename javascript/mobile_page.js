a681580df7ea7ea4f7e3bfd928cb1e47ee08ee38
Callum Anderson <callum.re.anderson@gmail.com>
2025-09-19 18:17:06 +0100

    oh no he did big bad move

--- javascript/mobile_page.js
+++ javascript/mobile_page.js
@@ -9,20 +9,26 @@ function enable_modible_mode(){
     document.getElementById("titledate").style.display = "none";
 
     //Body
-    document.getElementById("main").style.width = "90%";
+    /*document.getElementById("main").style.width = "90%";
     document.getElementById("main").style.paddingLeft ="5%";
-    document.getElementById("main").style.paddingRight = "5%";
+    document.getElementById("main").style.paddingRight = "5%";*/
+    document.getElementById("main").style = `
+        background-color: white;
+        padding-top: 1em;
+        padding-bottom: 1em;
+        margin: 0 10vw;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        font-size: 1rem;
+        width: 90%;
+        padding-left: 5%;
+        padding-right: 5%;;`
     document.getElementById("padding").style.width = "90%";
     document.getElementById("padding").style.paddingLeft ="5%";
     document.getElementById("padding").style.paddingRight = "5%";
 
 
- /*   main,
-  .padding {
-    width: 90%;
-    padding-left: 5%;
-    padding-right: 5%;
-  }*/
 }
 
 enable_modible_mode();
