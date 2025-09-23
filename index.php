6e4d5bbc380f07617c77af12031abbcbcac3c74e
Callum Anderson <callum.re.anderson@gmail.com>
2025-09-23 18:10:32 +0100

    Logos be wrong

--- index.php
+++ index.php
@@ -123,7 +123,7 @@ function printToolbar()
 	global $upage, $page, $action;
 
 	print "<div id=\"toolbar\">";
-	print "<div id=\"mobile-logo\"><a href=\"javascript:void(0);\" class=\"icon\" onclick=\"toggleDropdown()\"><img src=\"/w2/images/icon.png\"></a></div>";
+	print "<div id=\"mobile-logo\"><a href=\"javascript:void(0);\" class=\"icon\" onclick=\"toggleDropdown()\"><img src=\"" . ICON_URI . "/logo.png\"></a></div>";
 
 	# left hand side
 	print "<div id=\"left\">";	
